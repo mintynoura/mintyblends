1865540daabe1f2fdff4725a3ef9adbaad96410a
mintynoura <mintynoura@gmail.com>
2025-07-02 16:10:06 +0300

    0.6.1
    - Fixed startup error
    - Refactored compostables

--- src/main/java/io/github/mintynoura/mintyblends/registry/ModCompostables.java
+++ src/main/java/io/github/mintynoura/mintyblends/registry/ModCompostables.java
@@ -3,13 +3,19 @@ package io.github.mintynoura.mintyblends.registry;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.item.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ModCompostables {
-    public static List<Item> compost65;
-    public static List<Item> compost30;
+    public static List<Item> compost85 = new ArrayList<>();
+    public static List<Item> compost65 = new ArrayList<>();
+    public static List<Item> compost30 = new ArrayList<>();
 
     public static void addCompostables() {
+        compost85.add(ModBlocks.PURPLE_HORTENSIA.asItem());
+        compost85.add(ModBlocks.PINK_HORTENSIA.asItem());
+        compost85.add(ModBlocks.BLUE_HORTENSIA.asItem());
+
         compost65.add(ModBlocks.MINT.asItem());
         compost65.add(ModBlocks.CATNIP.asItem());
         compost65.add(ModBlocks.MEDICINAL_HERB.asItem());
@@ -18,9 +24,7 @@ public class ModCompostables {
         compost65.add(ModBlocks.CUREFLOWER.asItem());
         compost65.add(ModBlocks.RENDFLOWER.asItem());
         compost65.add(ModBlocks.SILENT_FLOWER.asItem());
-        compost65.add(ModBlocks.PURPLE_HORTENSIA.asItem());
-        compost65.add(ModBlocks.PINK_HORTENSIA.asItem());
-        compost65.add(ModBlocks.BLUE_HORTENSIA.asItem());
+
         compost30.add(ModItems.MINT_LEAVES);
         compost30.add(ModItems.CATNIP_LEAVES);
         compost30.add(ModItems.MEDICINAL_LEAVES);
@@ -29,13 +33,13 @@ public class ModCompostables {
         compost30.add(ModItems.HORTENSIA_SEEDS);
     }
 
-    public static void compostItems65() {
+    public static void compostItems() {
+        for (Item item : compost85) {
+            CompostingChanceRegistry.INSTANCE.add(item, 0.85f);
+        }
         for (Item item : compost65) {
             CompostingChanceRegistry.INSTANCE.add(item, 0.65f);
         }
-    }
-
-    public static void compostItems30() {
         for (Item item : compost30) {
             CompostingChanceRegistry.INSTANCE.add(item, 0.30f);
         }
@@ -43,7 +47,6 @@ public class ModCompostables {
 
     public static void registerCompostableItems() {
         addCompostables();
-        compostItems65();
-        compostItems30();
+        compostItems();
     }
 }
