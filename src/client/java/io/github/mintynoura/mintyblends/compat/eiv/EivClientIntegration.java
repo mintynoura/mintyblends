48cd0c0faf62b923a23d80b39918789df4450aa1
mintynoura <mintynoura@gmail.com>
2025-09-14 16:45:08 +0300

    fix crash on dedicated servers

--- src/client/java/io/github/mintynoura/mintyblends/compat/eiv/EivClientIntegration.java
+++ src/client/java/io/github/mintynoura/mintyblends/compat/eiv/EivClientIntegration.java
@@ -1,13 +1,11 @@
 package io.github.mintynoura.mintyblends.compat.eiv;
 
-import de.crafty.eiv.common.api.IExtendedItemViewIntegration;
 import de.crafty.eiv.common.api.recipe.ItemView;
 
 import java.util.Collections;
 
-public class EivClientIntegration implements IExtendedItemViewIntegration {
-    @Override
-    public void onIntegrationInitialize() {
+public class EivClientIntegration {
+    public static void onIntegrationInitialize() {
         ItemView.registerRecipeWrapper(KettleBrewingServerRecipe.TYPE, serverRecipe -> Collections.singletonList(new KettleBrewingViewRecipe(serverRecipe)));
     }
 }
